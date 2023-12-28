f52cb3f6e324c9b40956ab1b8a56aa3213415b36
aoki-h-jp <aoki-h-jp@users.noreply.github.com>
2023-12-28 10:38:52 +0000

    Apply Code Formatter Change

--- tests/test_downloader.py
+++ tests/test_downloader.py
@@ -4,23 +4,35 @@ TODO: download()の単体テスト
 """
 
 from datetime import datetime
+from unittest.mock import MagicMock, patch
 
 import pytest
 import requests
 import requests_mock
-from unittest.mock import patch, MagicMock
+
 from mexc_bulk_downloader.downloader import MexcBulkDownloader
-from mexc_bulk_downloader.exceptions import InvalidIntervalError, InvalidSymbolFormatError
+from mexc_bulk_downloader.exceptions import (InvalidIntervalError,
+                                             InvalidSymbolFormatError)
 
 risk_reverse_response = {
     "success": True,
     "code": 0,
-    "data":
-        [
-            {"symbol":"BTC_USDT","currency":"USDT","available":25236190.842658725020122405,"timestamp":1703750327370},
-            {"symbol":"ETH_USDT","currency":"USDT","available":10683529.154410919248666888,"timestamp":1703750327370}
-        ]
+    "data": [
+        {
+            "symbol": "BTC_USDT",
+            "currency": "USDT",
+            "available": 25236190.842658725020122405,
+            "timestamp": 1703750327370,
+        },
+        {
+            "symbol": "ETH_USDT",
+            "currency": "USDT",
+            "available": 10683529.154410919248666888,
+            "timestamp": 1703750327370,
+        },
+    ],
 }
+
 
 def test_make_interval():
     downloader = MexcBulkDownloader()
@@ -33,7 +45,10 @@ def test_make_interval():
 def test_validate_symbol():
     downloader = MexcBulkDownloader()
     with requests_mock.Mocker() as m:
-        m.get("https://contract.mexc.com/api/v1/contract/risk_reverse", json=risk_reverse_response)
+        m.get(
+            "https://contract.mexc.com/api/v1/contract/risk_reverse",
+            json=risk_reverse_response,
+        )
         assert downloader.validate_symbol("BTC_USDT") == "BTC_USDT"
         with pytest.raises(InvalidSymbolFormatError):
             downloader.validate_symbol("INVALID_SYMBOL")
@@ -42,7 +57,10 @@ def test_validate_symbol():
 def test_get_all_symbols_futures():
     downloader = MexcBulkDownloader()
     with requests_mock.Mocker() as m:
-        m.get("https://contract.mexc.com/api/v1/contract/risk_reverse", json=risk_reverse_response)
+        m.get(
+            "https://contract.mexc.com/api/v1/contract/risk_reverse",
+            json=risk_reverse_response,
+        )
         symbols = downloader.get_all_symbols_futures()
         assert "BTC_USDT" in symbols
         assert "ETH_USDT" in symbols
@@ -65,12 +83,13 @@ def test_get_all_symbols_futures_error(capsys):
         # シンボルリストが空であることを確認
         assert symbols is None
 
+
 def test_make_url_with_valid_symbol():
     # MexcBulkDownloader インスタンスの作成
     downloader = MexcBulkDownloader()
 
     # 正しいシンボルの場合、期待するURLを返すかテスト
-    with patch.object(downloader, 'validate_symbol', return_value="BTC_USDT"):
+    with patch.object(downloader, "validate_symbol", return_value="BTC_USDT"):
         expected_url = "https://contract.mexc.com/api/v1/contract/kline/BTC_USDT"
         assert downloader._make_url() == expected_url
 
@@ -80,18 +99,24 @@ def test_make_url_with_invalid_symbol():
     downloader = MexcBulkDownloader()
 
     # 不正なシンボルの場合、InvalidSymbolFormatError が発生するかテスト
-    with patch.object(downloader, 'validate_symbol', side_effect=InvalidSymbolFormatError("Invalid symbol: INVALID_SYMBOL")), pytest.raises(InvalidSymbolFormatError):
+    with patch.object(
+        downloader,
+        "validate_symbol",
+        side_effect=InvalidSymbolFormatError("Invalid symbol: INVALID_SYMBOL"),
+    ), pytest.raises(InvalidSymbolFormatError):
         downloader._make_url()
+
 
 def test_download_all():
     downloader = MexcBulkDownloader()
     test_symbols = ["BTC_USDT", "ETH_USDT"]
     interval = "1m"
 
-    with patch.object(downloader, 'get_all_symbols_futures', return_value=test_symbols), \
-         patch.object(downloader, 'download') as mock_download, \
-         patch("builtins.print") as mock_print:
-
+    with patch.object(
+        downloader, "get_all_symbols_futures", return_value=test_symbols
+    ), patch.object(downloader, "download") as mock_download, patch(
+        "builtins.print"
+    ) as mock_print:
         downloader.download_all(interval=interval)
 
         # 期待通りの回数だけdownloadメソッドが呼ばれることを確認
@@ -128,16 +153,16 @@ def test_execute_download():
     response_data = {
         "data": [
             {"time": start_date.timestamp(), "value": 100},
-            {"time": end_date.timestamp(), "value": 200}
+            {"time": end_date.timestamp(), "value": 200},
         ]
     }
 
     # モック化する関数やメソッド
-    with patch("requests.get") as mock_get, \
-         patch.object(downloader, '_make_url', return_value=expected_url), \
-         patch("pandas.DataFrame.to_csv") as mock_to_csv, \
-         patch("time.sleep") as mock_sleep:
-
+    with patch("requests.get") as mock_get, patch.object(
+        downloader, "_make_url", return_value=expected_url
+    ), patch("pandas.DataFrame.to_csv") as mock_to_csv, patch(
+        "time.sleep"
+    ) as mock_sleep:
         # requests.getのモックを設定
         mock_get.return_value = MagicMock(status_code=200, json=lambda: response_data)
 
@@ -163,10 +188,9 @@ def test_execute_download_non_200_response(capsys):
     interval = "1m"
     expected_url = f"https://contract.mexc.com/api/v1/contract/kline/{symbol}"
 
-    with patch("requests.get") as mock_get, \
-         patch.object(downloader, '_make_url', return_value=expected_url), \
-         patch("builtins.print") as mock_print:
-
+    with patch("requests.get") as mock_get, patch.object(
+        downloader, "_make_url", return_value=expected_url
+    ), patch("builtins.print") as mock_print:
         # 非200のステータスコードを返すように設定
         mock_get.return_value = MagicMock(status_code=404)
 
@@ -185,11 +209,9 @@ def test_execute_download_connection_error(capsys):
     interval = "1m"
     expected_url = f"https://contract.mexc.com/api/v1/contract/kline/{symbol}"
 
-    with patch("requests.get", side_effect=requests.ConnectionError), \
-         patch.object(downloader, '_make_url', return_value=expected_url), \
-         patch("builtins.print") as mock_print, \
-         patch("time.sleep") as mock_sleep:
-
+    with patch("requests.get", side_effect=requests.ConnectionError), patch.object(
+        downloader, "_make_url", return_value=expected_url
+    ), patch("builtins.print") as mock_print, patch("time.sleep") as mock_sleep:
         downloader.execute_download(symbol, start_date, end_date, interval)
 
         # 標準出力をキャプチャして確認
