020a91eeb68b4b00190349cd52ca7ee13653ab7c
aoki-h-jp <aoki-h-jp@users.noreply.github.com>
2023-12-28 07:50:36 +0000

    Apply Code Formatter Change

--- example/download.py
+++ example/download.py
@@ -1,13 +1,17 @@
-from mexc_bulk_downloader.downloader import MexcBulkDownloader
 from datetime import datetime
+
+from mexc_bulk_downloader.downloader import MexcBulkDownloader
 
 downloader = MexcBulkDownloader(destination_dir=".")
 start_time = datetime(2023, 1, 1)
 end_time = datetime(2023, 2, 1)
 
 # Download
-downloader.download(symbol="BTC_USDT", start_date=start_time, end_date=end_time, interval="1m")
+downloader.download(
+    symbol="BTC_USDT", start_date=start_time, end_date=end_time, interval="1m"
+)
 
 # interval = 1h
-downloader.download(symbol="BTC_USDT", start_date=start_time, end_date=end_time, interval="1h")
-
+downloader.download(
+    symbol="BTC_USDT", start_date=start_time, end_date=end_time, interval="1h"
+)
