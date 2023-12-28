f51970e8a12de29e5cb5f15735b6399c65966729
Hirotaka Aoki <113173839+aoki-h-jp@users.noreply.github.com>
2023-12-28 16:50:11 +0900

    add example, todo

---
+++ example/download.py
@@ -0,0 +1,13 @@
+from mexc_bulk_downloader.downloader import MexcBulkDownloader
+from datetime import datetime
+
+downloader = MexcBulkDownloader(destination_dir=".")
+start_time = datetime(2023, 1, 1)
+end_time = datetime(2023, 2, 1)
+
+# Download
+downloader.download(symbol="BTC_USDT", start_date=start_time, end_date=end_time, interval="1m")
+
+# interval = 1h
+downloader.download(symbol="BTC_USDT", start_date=start_time, end_date=end_time, interval="1h")
+
